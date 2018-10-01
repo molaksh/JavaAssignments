3fe5753ed79c54f8cb7971bae93d595cd1d24e90
molaksh <mohan.kris.lakshmanan@gmail.com>
2018-10-01 18:43:30 -0400

    3

---
+++ EnhancedFor.java
@@ -0,0 +1,12 @@
+class EnhancedFor{
+    public static void main(String[] args) {
+        int[] arr = {3,4,5,6,7,7,8};
+        int total =0;
+
+        for (int x : arr) {
+            System.out.println("x = "+x);
+            total += x;
+        }
+        System.out.println("sum of all values in array is "+ total);
+    }
+}
