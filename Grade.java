f69ed276554a096e28bb0f7339c8cca65c76f4dc
molaksh <mohan.kris.lakshmanan@gmail.com>
2018-10-01 17:55:03 -0400

    2

---
+++ Grade.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+class Grade{
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.println("enter value between 1 to 100");
+        int num = s.nextInt();
+        s.close();
+        int grade = (int) num/10;
+        System.out.println("Grade "+grade);
+
+        if(grade == 10){System.out.println("Perfect Score");}
+        else if(grade == 9){System.out.println("Excellent");}
+        else if(grade == 8){System.out.println("Nice Job");}
+        else if(grade == 7){System.out.println("Average");}
+        else if(grade < 7){System.out.println("Below Average");}
+
+
+    }
+}
---
+++ NumReverse.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+class NumReverse{
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        while(true){
+            System.out.print("enter your number: ");
+            String input = s.next();
+            if(input.equals("quit")){break;}
+            try {
+                int inp = Integer.parseInt(input);
+          
+            
+                int reminder, rev =0;
+                while(inp>0){
+                    reminder = (int) inp %10;
+                    rev = (rev*10) + reminder;
+                    inp = (int) inp/10; 
+                }
+            
+                System.out.println("Your number is "+ input);
+                System.out.println("Reverse number is "+ rev);
+                System.out.println();
+            } catch (Exception e) {
+                System.out.println("enter an integer");continue;}
+            
+
+        }
+    }
+}
