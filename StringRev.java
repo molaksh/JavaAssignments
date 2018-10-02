91f405e8a7849c5a0c86523ccbf897172ace3be8
molaksh <mohan.kris.lakshmanan@gmail.com>
2018-10-01 20:27:23 -0400

    a

---
+++ StringRev.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+class StringRev{
+    public static void main(String[] args) {
+
+        while(true){
+            Scanner s = new Scanner(System.in); 
+            String str = s.next();
+            
+            String rev = reverse(str);
+            System.out.println( rev);
+        }
+    }
+
+    public static String reverse(String s){
+        String rev = "";
+        int strLength = s.length();
+        for (int i = strLength -1; i >= 0; i--) {
+            rev=rev+s.charAt(i);
+        }    
+        return rev;
+        }
+            
+        
+}
