f5510c859a27e5419c14d8033ec5bc771f43b796
molaksh <mohan.kris.lakshmanan@gmail.com>
2018-10-05 07:56:23 -0400

    a

--- setTwo/Six.java
+++ setTwo/Six.java
@@ -1,5 +1,21 @@
-package setTwo;
+//package setTwo;
+
+import java.util.Scanner;
 
 class Six{
+    public static void main(String[] args) {
+        double salesTax = 8.25/100;
+        Scanner s = new Scanner(System.in);
+
+        System.out.println("Enter the price: ");
+        double price = s.nextDouble();
+        double salesTaxAmount = price * salesTax;
+        double finalAmount = price + salesTaxAmount;
+
+        System.out.println("Price = "+ price);
+        System.out.println("Sales Tax = "+ salesTaxAmount);
+        System.out.println("Total Price = "+finalAmount);
+    }
     
+
 }
