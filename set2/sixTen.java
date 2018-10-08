26c405996ae4593993dcc2ec0f1512f5dc2dd0e4
Mohan <mohan.kris.lakshmanan@gmail.com>
2018-10-07 20:25:54 -0400

    a

---
+++ set2/sixTen.java
@@ -0,0 +1,26 @@
+package set2;
+
+import java.util.Scanner;
+
+public class sixTen {
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		String str = s.nextLine();
+		s.close();
+		
+		System.out.println(capitalize(str));
+		
+	}
+	
+	public static String capitalize(String str) {
+		
+		String[] arr = str.split(" ");
+	    StringBuffer sb = new StringBuffer();
+
+	    for (int i = 0; i < arr.length; i++) {
+	        sb.append(Character.toUpperCase(arr[i].charAt(0)))
+	            .append(arr[i].substring(1)).append(" ");
+	    }          
+	    return sb.toString().trim();
+	}
+}
