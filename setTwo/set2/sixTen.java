b4cc4572bc36f0cd851794cd4df4459bb1d73d65
Mohan <mohan.kris.lakshmanan@gmail.com>
2018-10-08 11:59:36 -0400

    a

--- setTwo/set2/sixTen.java
+++ setTwo/set2/sixTen.java
@@ -5,8 +5,10 @@ import java.util.Scanner;
 public class sixTen {
 	public static void main(String[] args) {
 		Scanner s = new Scanner(System.in);
+		System.out.println("Enter sentence to capitalize");
 		String str = s.nextLine();
 		s.close();
+		
 		
 		System.out.println(capitalize(str));
 		
