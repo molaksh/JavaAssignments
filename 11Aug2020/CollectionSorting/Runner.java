51716d62462acddd3ebf84b7cf763b8f7fdb1db4
Mohan <mohan.kris.lakshmanan@gmail.com>
2020-08-11 16:14:40 -0400

    commit

--- 11Aug2020/CollectionSorting/Runner.java
+++ 11Aug2020/CollectionSorting/Runner.java
@@ -1,31 +1,28 @@
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 
 public class Runner {
   public static void main(String[] args) {
-    
+
     List<Animal> animalList = new ArrayList<>();
 
     animalList.add(new Animal("Lion", 5, "Land Animal"));
     animalList.add(new Animal("Frog", 2, "Amphibian"));
     animalList.add(new Animal("Fish", 1, "Aquatic"));
 
-
-    //Collections.sort(animalList);
+    // Collections.sort(animalList);
 
     animalList.forEach(System.out::println);
 
-    animalList.sort(Comparator.comparing(Animal::getEnvironment)
-    .thenComparing(Animal::getKind).thenComparingInt(Animal::getSpeed));
-    //.thenComparing(Animal::getKind()).thenComparing(Animal::getSpeed());
-  
+    animalList.sort(
+        Comparator.comparing(Animal::getEnvironment).thenComparing(Animal::getKind).thenComparingInt(Animal::getSpeed));
+
     animalList.sort(Comparator.comparingInt(Animal::getSpeed));
     animalList.sort(Comparator.comparing(Animal::getEnvironment));
     animalList.sort(Comparator.comparing(Animal::getKind));
 
     animalList.forEach(System.out::println);
-    
+
   }
 }
